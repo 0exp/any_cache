8e1ad496300d09b4ad12ed215c89dc884d1ebcdd
Rustam Ibragimov <iamdaiver@gmail.com>
2018-09-01 20:59:53 +0300

    [operation/exist?] existence spec with temporal entries

--- spec/features/exist_spec.rb
+++ spec/features/exist_spec.rb
@@ -7,6 +7,18 @@ describe 'Operation: #exist?' do
   let(:expiration_time) { 8 } # NOTE: in seconds
   let(:entry)           { { key: SecureRandom.hex, value: SecureRandom.hex(4) } }
 
+  context 'entry with expiration time' do
+    before { cache_store.write(entry[:key], entry[:value], expires_in: expiration_time) }
+
+    specify 'returns corresponding value (exists => true, does not exist => false)' do
+      expect(cache_store.exist?(entry[:key])).to eq(true)
+
+      sleep(expiration_time + 1)
+
+      expect(cache_store.exist?(entry[:key])).to eq(false)
+    end
+  end
+
   context 'when entry exists' do
     before { cache_store.write(entry[:key], entry[:value]) }
 
