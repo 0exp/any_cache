869f3d469296893d8bebb24b1ceb2ac691c6639e
Rustam Ibragimov <iamdaiver@gmail.com>
2018-09-07 00:54:00 +0300

    [logging] better specs

--- spec/features/logging_spec.rb
+++ spec/features/logging_spec.rb
@@ -38,60 +38,89 @@ describe 'Feature: Logging' do
     let(:cacher_name) { cache_store.class.name }
 
     specify '#read' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<read>]"
+
+      expect(output.string).not_to include(log_message)
       cache_store.read(entry[:key], { custom_option: SecureRandom.hex(4) })
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<read>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#write' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<write>]"
+
+      expect(output.string).not_to include(log_message)
       cache_store.write(entry[:key], entry[:value], { expires_in: expires_in })
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<write>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#delete' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<delete>]"
+
+      expect(output.string).not_to include(log_message)
       cache_store.delete(entry[:key], { custom_option: SecureRandom.hex(4) })
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<delete>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#increment' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<increment>]"
       cache_store.write(entry[:key], rand(2..10))
+
+      expect(output.string).not_to include(log_message)
       cache_store.increment(entry[:key], rand(1..2), { expires_in: expires_in })
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<increment>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#decrmenet' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<decrement>]"
       cache_store.write(entry[:key], rand(2..10))
+
+      expect(output.string).not_to include(log_message)
       cache_store.decrement(entry[:key], rand(1..2), expires_in: expires_in)
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<decrement>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#expire' do
       cache_store.write(entry[:key], entry[:value])
+      log_message = "[AnyCache<#{cacher_name}>/Activity<expire>]"
+
+      expect(output.string).not_to include(log_message)
       cache_store.expire(entry[:key], expires_in: expires_in)
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<expire>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#persist' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<persist>]"
       cache_store.write(entry[:key], entry[:value], expires_in: expires_in)
+
+      expect(output.string).not_to include(log_message)
       cache_store.persist(entry[:key])
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<persist>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#clear' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<clear>]"
+
+      expect(output.string).not_to include(log_message)
       cache_store.clear(custom_option: SecureRandom.hex(4))
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<clear>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#exist?' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<exist?>]"
+
+      expect(output.string).not_to include(log_message)
       cache_store.exist?(entry[:key], custom_option: SecureRandom.hex(4))
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<exist?>]")
+      expect(output.string).to include(log_message)
     end
 
     specify '#fetch' do
+      log_message = "[AnyCache<#{cacher_name}>/Activity<fetch>]"
+
+      expect(output.string).not_to include(log_message)
       cache_store.fetch(entry[:key], force: [true, false].sample, expires_in: expires_in) do
         entry[:value]
       end
-
-      expect(output.string).to include("[AnyCache<#{cacher_name}>/Activity<fetch>]")
+      expect(output.string).to include(log_message)
     end
   end
 end
