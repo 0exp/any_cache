bca6cdc60bc4ebbe72b340d0651558b67e3006d6
Rustam Ibragimov <iamdaiver@gmail.com>
2018-09-07 01:49:24 +0300

    [logging] refactoring (separated files for Logging::Activity and Logging::Logger)

--- lib/any_cache/logging.rb
+++ lib/any_cache/logging.rb
@@ -1,40 +1,6 @@
 # frozen_string_literal: true
 
 module AnyCache::Logging
-  # @api private
-  # @since 0.3.0
-  class Logger < ::Logger
-    # @api private
-    # @since 0.3.0
-    def initialize(*, **)
-      super
-      self.level = ::Logger::INFO
-    end
-  end
-
-  # @api private
-  # @since 0.3.0
-  module Activity
-    # @return [String]
-    #
-    # @api private
-    # @since 0.3.0
-    ANONYMOUS_CACHER_CLASS_NAME = '<__anonymous_cache__>'
-
-    class << self
-      # @param cacher [AnyCache]
-      # @param logger [::Logger]
-      # @option activity [String, NilClass]
-      # @param message [String, NillClass]
-      # @return [void]
-      #
-      # @api private
-      # @since 0.3.0
-      def log(cacher, logger, activity: nil, message: nil)
-        cacher = cacher.class.name || ANONYMOUS_CACHER_CLASS_NAME
-        progname = "[AnyCache<#{cacher}>/Activity<#{activity}>]"
-        logger.add(logger.level, message, progname)
-      end
-    end
-  end
+  require_relative 'logging/logger'
+  require_relative 'logging/activity'
 end
---
+++ lib/any_cache/logging/activity.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# @api private
+# @since 0.3.0
+module AnyCache::Logging::Activity
+  # @return [String]
+  #
+  # @api private
+  # @since 0.3.0
+  ANONYMOUS_CACHER_CLASS_NAME = '<__anonymous_cache__>'
+
+  class << self
+    # @param cacher [AnyCache]
+    # @param logger [::Logger]
+    # @option activity [String, NilClass]
+    # @param message [String, NillClass]
+    # @return [void]
+    #
+    # @api private
+    # @since 0.3.0
+    def log(cacher, logger, activity: nil, message: nil)
+      cacher = cacher.class.name || ANONYMOUS_CACHER_CLASS_NAME
+      progname = "[AnyCache<#{cacher}>/Activity<#{activity}>]"
+      logger.add(logger.level, message, progname)
+    end
+  end
+end
