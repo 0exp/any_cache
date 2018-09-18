6fb0d4e59593d0b26df402597645befc8750569c
Rustam Ibragimov <iamdaiver@gmail.com>
2018-09-18 23:29:00 +0300

    [multi-operations] default #delete_matched behaviour for activesupport naive store

--- lib/any_cache/adapters/active_support_naive_store.rb
+++ lib/any_cache/adapters/active_support_naive_store.rb
@@ -58,14 +58,14 @@ module AnyCache::Adapters
       lock.with_write_lock { super }
     end
 
-    # @param pattern [???]
-    # @param options [???]
+    # @param pattern [Object]
+    # @param options [Hash]
     # @return [void]
     #
     # @api private
     # @since 0.3.0
     def delete_matched(pattern, **options)
-
+      lock.with_write_lock { super }
     end
 
     # @param options [Hash]
